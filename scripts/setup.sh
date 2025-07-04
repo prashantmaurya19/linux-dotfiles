4c4cbddef5f7b9d2992cc51f07c60a561654c15c
Prashant Maurya <prashantmaurya1009@gmail.com>
2025-07-04 16:17:48 +0530

    dev : some changes

---
+++ scripts/setup.sh
@@ -0,0 +1,15 @@
+
+
+sudo apt update && apt upgrade
+sudo apt install wezterm wlogout sway swaybg -y 
+sudo apt install pulseaudio pulseaudio-utils -y
+sudo snap install yazi --classic
+sudo apt install redshift -y
+
+# manual install :-
+# zig
+
+# fc-list | grep "Fira Mono Nerd Font" 
+# install wezterm
+
+
